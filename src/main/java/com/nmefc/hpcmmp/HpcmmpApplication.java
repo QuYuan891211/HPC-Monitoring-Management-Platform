48b6e32f619ddfb43e60ab6f9edd47a5cf75fa72
quyuan <quyuan891211@163.com>
2019-01-30 14:34:20 +0800

    Create project

---
+++ src/main/java/com/nmefc/hpcmmp/HpcmmpApplication.java
@@ -0,0 +1,14 @@
+package com.nmefc.hpcmmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcmmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpcmmpApplication.class, args);
+	}
+
+}
+
