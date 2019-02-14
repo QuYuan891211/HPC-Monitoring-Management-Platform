dde779067e5ccf50a5f560fdeccf1fb298992852
quyuan <quyuan891211@163.com>
2019-02-14 16:54:54 +0800

    universal method Extraction--------Dao

--- src/main/java/com/nmefc/hpcmmp/dao/TestDao.java
+++ src/main/java/com/nmefc/hpcmmp/dao/TestDao.java
@@ -10,23 +10,6 @@ import java.util.List;
  * @Date: Created in 17:45 2019/1/30
  * @Modified By:
  */
-public interface TestDao {
-    /**
-     *
-     * @Description: select all test
-     *
-     * @auther: QuYuan
-     * @date: 8:47 2019/1/31
-     * @param: []
-     * @return: java.util.List<com.nmefc.hpcmmp.entity.Test>
-     *
-     */
-    List<Test> selectAll();
-/**
- * @author: QuYuan
- * @date: 11:40 2019/1/31 
- * @param: [test]
- * @return: int
- */
-    int insert(Test test);
+public interface TestDao<Test> extends BaseDao<Test> {
+
 }
