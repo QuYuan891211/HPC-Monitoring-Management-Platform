ee04106496ea5c9c1c31555a12d1196ea8c01052
quyuan <quyuan891211@163.com>
2019-05-10 16:52:45 +0800

    工作流引擎架构

--- src/main/java/com/nmefc/hpcmmp/HpcmmpApplication.java
+++ src/main/java/com/nmefc/hpcmmp/HpcmmpApplication.java
@@ -1,13 +1,19 @@
 package com.nmefc.hpcmmp;
 
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 
 @SpringBootApplication
 @EnableTransactionManagement
